c37822e5cb0b9c1467751a09fac6235cf0c2608b
Ribya Ann Thomas <ribtom16@gmail.com>
2023-05-07 16:49:17 +0530

    added sessions, better ui and  updated tests

--- app.py
+++ app.py
@@ -1,37 +1,47 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 import openai
+import uuid
 import key
 openai.api_key = key.API_KEY
 app = Flask(__name__)
-global messages
-# To give gpt an initial context
-messages = [{'role': 'system', 'content': 'Give response using the context given'}, {
-    'role': 'assistant', 'content': 'What is your question or prompt?'}]
+app.secret_key = 'secret_riantom'
+app.config['SESSION_TYPE'] = 'filesystem'
+# define the Flask app routes
 
 
-def gpt_response(message):
+def gpt_response(message, session_id):
+    messages = session.get(session_id, [{'role': 'system', 'content': 'Give response using the context given'}, {
+                           'role': 'assistant', 'content': 'What is your question or prompt?'}])  # Retrieve the context for the session ID
     prompt = {'role': 'user', 'content': message}
     messages.append(prompt)  # to get responses with context
     completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo", messages=messages)
     if completion.choices[0].message != None:
         messages.append(dict(completion.choices[0].message))
+        # Store the updated context for the session ID
+        session[session_id] = messages
         return completion.choices[0].message['content']
     else:
         return 'Failed to Generate response!'
 
-# define the Flask app routes
-
 
 @app.route("/")
 def index():
+    if 'session_id' not in session:
+        # Generate a new session ID if it doesn't exist
+        session['session_id'] = str(uuid.uuid4())
     return render_template("index.html")
 
 
 @app.route("/result", methods=["POST"])
 def result():
     message = request.form['message']
-    response = gpt_response(message)
+    if session:
+        session_id = session['session_id']
+    else:
+        # for testing to work for the result only
+        session_id = request.args.get('session_id')
+    response = gpt_response(message, session_id)
     return render_template('result.html', message=message, response=response)
 
 
--- test_app.py
+++ test_app.py
@@ -1,4 +1,6 @@
 import unittest
+import uuid
+
 from app import app, gpt_response
 
 
@@ -6,18 +8,29 @@ class TestApp(unittest.TestCase):
     def setUp(self):
         app.config['TESTING'] = True
         self.app = app.test_client()
+        self.app.session_id = str(uuid.uuid4())
+        # self.context = [{'role': 'system', 'content': 'Give response using the context given'},
+        #                 {'role': 'assistant', 'content': 'What is your question or prompt?'}]
 
-    def test_index(self):  # testing for index page
-        response = self.app.get('/')
-        self.assertEqual(response.status_code, 200)
+    def tearDown(self):
+        self.context = None
+        self.app.session_id = None
+
+    def test_index(self):
+        with app.test_request_context():  # testing for index page
+            response = self.app.get('/')
+            self.assertEqual(response.status_code, 200)
 
     def test_result(self):  # test for result page
-        response = self.app.post('/result', data={'message': 'test message'})
-        self.assertEqual(response.status_code, 200)
+        with self.app as client:
+            response = client.post(
+                '/result', data={'message': 'test message'})
+            self.assertEqual(response.status_code, 200)
 
     def test_gpt_response(self):  # test for gpt api integration
-        response = gpt_response('Hello')
-        self.assertIsNotNone(response)
+        with app.test_request_context():
+            response = gpt_response('Hello', self.app.session_id)
+            self.assertIsNotNone(response)
 
     def test_valid_prompt(self):  # test for valid response for prompts
         with self.app as client:
